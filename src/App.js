551e319dc292b1635ab4f25e8db3de8f5c8b9b47
Smitasubhro <Subhrosmita.Paul@in.ey.com>
2025-01-28 01:32:22 +0530

    changed in App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import { Routes, Route } from "react-router-dom"
 import {Navbar} from './Components/Navbar';
 import Footer from './Components/Footer';
 // import Sidebar from './Components/Sidebar';
-import UseCaseDetail from './Pages/UseCaseDetail';
-import AssetDetail from './Pages/AssetDetail';
 const Home=lazy(()=>import('./Pages/Home'));
 const Usecase=lazy(()=>import('./Pages/Usecase'));
 const Zone=lazy(()=>import('./Pages/Zone'));
 const Assets=lazy(()=>import('./Pages/Assets'));
-// const UseCaseDetail=lazy(()=>import('./Pages/UseCaseDetail'));
-// const AssetDetail=lazy(()=>import('./Pages/AssetDetail'));
+const UseCaseDetail=lazy(()=>import('./Pages/UseCaseDetail'));
+const AssetDetail=lazy(()=>import('./Pages/AssetDetail'));
 const AddUseCase=lazy(()=>import('./Pages/AddUseCase'));
 const AddAsset=lazy(()=>import('./Pages/AddAsset'));
 
@@ -27,10 +25,10 @@ function App() {
         <Route path="/zone" element={<Suspense fallback={<div>Loading....</div>}><Zone /></Suspense>} />
         <Route path="/usecases" element={<Suspense fallback={<div>Loading....</div>}><Usecase /></Suspense> }/>
         <Route path="/assets" element={<Suspense fallback={<div>Loading....</div>}><Assets /></Suspense>} />
-        <Route path="/usecases/:caseid/:casetitle" element={<UseCaseDetail />} />
+        <Route path="/usecases/:caseid/:casetitle" element={<Suspense fallback={<div>Loading....</div>}><UseCaseDetail /></Suspense>} />
         <Route path="/addusecase" element={<Suspense fallback={<div>Loading....</div>}><AddUseCase /></Suspense>} />
         <Route path="/addasset" element={<Suspense fallback={<div>Loading....</div>}><AddAsset /></Suspense>} />
-        <Route path="/assets/:assetid/:assettitle" element={<AssetDetail />} />
+        <Route path="/assets/:assetid/:assettitle" element={<Suspense fallback={<div>Loading....</div>}><AssetDetail /></Suspense>} />
       </Routes>
       <Footer/>
     </div>
